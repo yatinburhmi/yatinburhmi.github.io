c22f302be6aee5bedbeabd2d0d3658bbd332fade
yatinburhmi <yatinburhmi@gmail.com>
2025-03-13 17:42:42 -0400

    feat: Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Main from './containers'
+
+function App() {
+
+  return (
+    <div>
+      <Main/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/index.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/header/header";
+
+const Main = () => {
+    return (
+        <>
+        <Header/>
+        </>
+    )
+}
+
+export default Main;
