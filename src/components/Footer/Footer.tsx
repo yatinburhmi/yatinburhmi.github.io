d85e2c03cfdf4585a284f983410288b5c2d44820
yatinburhmi <yatinburhmi@gmail.com>
2025-03-19 15:07:04 -0400

    fix: Github pages deployment issues

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,9 +2,6 @@ import { Container } from "./styles";
 import reactIcon from "../../assets/react-icon.svg";
 import linkedin from "../../assets/linkedin.svg";
 import githubIcon from "../../assets/github.svg";
-import whatsapp from "../../assets/whatsapp.svg";
-import telegram from "../../assets/telegram.svg";
-import instagramIcon from "../../assets/instagram.svg";
 import {
   YATIN_BURHMI_GITHUB,
   YATIN_BURHMI_LINKEDIN,
